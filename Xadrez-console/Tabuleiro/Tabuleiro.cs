475c1c399e73bafa1eebb8df4000ec37c86c7ac2
Cristiano ramos <cristianorag@gmail.com>
2021-09-30 19:37:55 -0300

    Incluindo a classe TabelaException

--- Xadrez-console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,5 +1,5 @@
-﻿namespace tabuleiro{
-    class Tabuleiro{
+﻿namespace tabuleiro {
+    class Tabuleiro {
         public int linhas { get; set; }
         public int colunas { get; set; }
         private Peca[,] pecas;
@@ -9,13 +9,31 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
-        public Peca peca(int linha, int coluna){
+        public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
-        public void colocarPeca(Peca p, Posicao pos){
+        public Peca peca(Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+
+        }
+        public bool colocarPeca(Posicao pos) {
+            return peca(pos) != null;
+        }
+        public void colocarPeca(Peca p, Posicao pos) {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        public bool posicaoValida(Posicao pos) {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas) {
+                return false;
+            }
+            return true;
 
+        }
+        public void validarPosicao(Posicao pos ) {
+            if (!posicaoValida(pos)) {
+                throw new TabuleiroException("Posição invalida!");
+            }
         }
     }
 }
---
+++ Xadrez-console/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xadrez_console.Tabuleiro {
+    class TabuleiroException {
+    }
+}
