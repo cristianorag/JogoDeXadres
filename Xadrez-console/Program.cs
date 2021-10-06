debb66657142f31a3854149c527332c00671a2f7
Cristiano ramos <cristianorag@gmail.com>
2021-10-06 19:19:05 -0300

    Criando a PosicaoXadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -2,24 +2,16 @@
 using tabuleiro;
 using xadrez;
 
-namespace Xadrez_console{
-    class Program{
-        static void Main(string[] args){
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-               
-               
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+namespace Xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
             Console.ReadLine();
 
-            
+
         }
     }
 }
